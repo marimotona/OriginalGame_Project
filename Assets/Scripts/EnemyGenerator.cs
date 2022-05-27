7010364dd58f6491ecb678e79b3a8c7877b64ca7
marimotona <ssmmo.06021224@gmail.com>
2022-05-27 14:34:29 +0900

    敵の自動生成

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMove : MonoBehaviour
+public class EnemyGenerator : MonoBehaviour
 {
+
+    public GameObject enemyPrefabs;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(enemyPrefabs, transform.position, transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position -= new Vector3(0, Time.deltaTime, 0);
+        
     }
 }
