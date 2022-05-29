7309f78b55d8d87592527e93a9ecae4d899c8c7e
marimotona <ssmmo.06021224@gmail.com>
2022-05-29 11:27:49 +0900

    弾の爆発の作成

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+
+    public GameObject explosion;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,8 @@ public class EnemyMove : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Instantiate(explosion, transform.position, transform.rotation);
+
         Destroy(gameObject);
         Destroy(collision.gameObject);
     }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -33,8 +33,8 @@ public class PlayerMove : MonoBehaviour
     {
 
         inputDirection = new Vector3(
-            Input.GetAxis("Horizontal"),
-            Input.GetAxis("Vertical")
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
             ,0
 
             );
