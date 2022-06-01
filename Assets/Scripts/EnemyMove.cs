ae6b431b8f139750f89f740b9134f73c4553428f
marimotona <ssmmo.06021224@gmail.com>
2022-06-01 15:49:54 +0900

    スコアの加算実装

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -7,10 +7,12 @@ public class EnemyMove : MonoBehaviour
 
     public GameObject explosion;
 
+    GameController gameController;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameController = GameObject.Find("GameController").GetComponent<GameController>();
     }
 
     // Update is called once per frame
@@ -25,5 +27,7 @@ public class EnemyMove : MonoBehaviour
 
         Destroy(gameObject);
         Destroy(collision.gameObject);
+
+        gameController.AddScore();
     }
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public Text scoreText;
+    int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "SCORE : " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore()
+    {
+        score += 100;
+        scoreText.text = "SCORE : " + score;
+    }
+}
