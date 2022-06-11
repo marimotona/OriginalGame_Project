5c7cbc1e0052057d00b2ae5fb98d93e973103647
marimotona <ssmmo.06021224@gmail.com>
2022-06-11 22:14:55 +0900

    スタートの効果音とタイトル画面のデザイン実装

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -6,19 +6,42 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+
+    AudioSource audioSource;
+    public AudioClip startSE;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+
+        //StartCoroutine(DelayCoroutine());
+        //Invoke("Update", 5.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            audioSource.PlayOneShot(startSE);
+            SceneManager.LoadScene("Main");
+            
+        }
+                
+    }
+
+    /*
+    private IEnumerator DelayCoroutine()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.PlayOneShot(startSE);
+            yield return new WaitForSeconds(3);
             SceneManager.LoadScene("Main");
 
         }
     }
+    */
 }
