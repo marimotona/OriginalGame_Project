e41aa532ee9fcacdc5990515b9309653c56cc6c6
marimotona <ssmmo.06021224@gmail.com>
2022-06-13 17:44:46 +0900

    プレイヤーの移動方法の修正と移動範囲制限

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,9 +7,9 @@ public class PlayerMove : MonoBehaviour
 
     Rigidbody2D playerRigidbody;
 
-    Vector3 inputDirection;
+    //Vector3 inputDirection;
 
-    float moveSpeed = 4.0f;
+    //float moveSpeed = 4.0f;
 
     public Transform sparkPoint; //‹…‚ð”­ŽË‚·‚éˆÊ’u
 
@@ -24,11 +24,11 @@ public class PlayerMove : MonoBehaviour
 
     public AudioClip scoreSE;
 
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        inputDirection = new Vector3(1, 0, 0);
+        //inputDirection = new Vector3(1, 0, 0);
 
         playerRigidbody = this.gameObject.GetComponent<Rigidbody2D>();
 
@@ -41,12 +41,29 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+
+        Vector3 nextPosition = transform.position + new Vector3(x, y, 0) * Time.deltaTime * 4.0f;
+        nextPosition = new Vector3(
+            Mathf.Clamp(nextPosition.x, -8.5f, 8.5f),
+            Mathf.Clamp(nextPosition.y, -4.5f, 4.5f),
+            nextPosition.z
+            );
+
+        transform.position = nextPosition;
+
+
+        //transform.position += new Vector3(x, y, 0) * Time.deltaTime * 4.0f;
+        /*
         inputDirection = new Vector3(
             Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical"),
             0
             );
-        
+        */
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(sparkPrefab, sparkPoint.position, Quaternion.Euler(0, 0, 90));
@@ -57,10 +74,12 @@ public class PlayerMove : MonoBehaviour
 
     }
 
+    /*
     private void FixedUpdate()
     {
         playerRigidbody.velocity = inputDirection * moveSpeed;
     }
+    */
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
